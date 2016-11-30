0f7dc84b066ebbe504dda9ca5f31c8870ebe6716
Chris Severns <severnsc@gmail.com>
2016-11-30 16:39:10 -0500

    Update board and spec for initialize

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,65 @@
+require_relative "./pieces.rb"
+
 class Board
+
+	attr_reader :squares, :pawns, :rooks, :knights, :bishops, :queens, :kings
 
 	@@x_axis = ("a".."h").to_a
 	@@y_axis = (1..8).to_a
 
 	def initialize
+		@pieces = [@pawns, @rooks, @knights, @bishops, @queens, @kings]
+		@pieces.each {|piece| piece = []}
+		create_pawns("black")
+		create_pawns("white")
+		create_rooks("black")
+		create_rooks("white")
+		create_bishops("black")
+		create_bishops("white")
+		create_knights("black")
+		create_knights("white")
+		create_queen("black")
+		create_queen("white")
+		create_king("black")
+		create_king("white")
+		@squares = Hash.new
+		@@x_axis.each do |x|
+			@@y_axis.each do |y|
+				@squares[x+y.to_s] = ""
+			end
+		end
+		black_pawn_squares = ["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"]
+		black_pawns = pawns.select {|pawn| pawn.color == "black"}
+		black_pawn_squares.each_with_index {|sq, index| @squares[sq] = black_pawns[index]}
+		white_pawn_squares = ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"]
+		white_pawns = pawns.select {|pawn| pawn.color == "white"}
+		white_pawn_squares.each_with_index {|sq, index| @squares[sq] = white_pawns[index]}
+		black_rook_squares = ["A8", "H8"]
+		black_rooks = rooks.select {|rook| rook.color == "black"}
+		black_rook_squares.each_with_index {|sq, index| @squares[sq] = black_rooks[index]}
+		white_rook_squares = ["A1", "H1"]
+		white_rooks = rooks.select {|rook| rook.color == "white"}
+		white_rook_squares.each_with_index {|sq, index| @squares[sq] = white_rooks[index]}
+		black_knight_squares = ["B8", "G8"]
+		black_knights = knights.select {|knight| knight.color == "black"}
+		black_knight_squares.each_with_index {|sq, index| @squares[sq] = black_knights[index]}
+		white_knight_squares = ["B1", "G1"]
+		white_knights = knights.select {|knight| knight.color == "white"}
+		white_knight_squares.each_with_index {|sq, index| @squares[sq] = white_knights[index]}
+		black_bishop_squares = ["C8", "F8"]
+		black_bishops = bishops.select {|bishop| bishop.color == "black"}
+		black_bishop_squares.each_with_index {|sq, index| @squares[sq] = black_bishops[index]}
+		white_bishop_squares = ["C1", "F1"]
+		white_bishops = bishops.select {|bishop| bishop.color == "white"}
+		white_bishop_squares.each_with_index {|sq, index| @squares[sq] = white_bishops[index]}
+		black_queen = queens.select {|queen| queen.color == "black"}
+		@squares["D8"] = black_queen[0]
+		white_queen = queens.select {|queen| queen.color == "white"}
+		@squares["D1"] = white_queen[0]
+		black_king = kings.select {|king| king.color == "black"}
+		@squares["E8"] = black_king[0]
+		white_king = kings.select {|king| king.color == "white"}
+		@squares["E1"] = white_king
 	end
 
 	def x_axis
@@ -14,5 +70,34 @@ class Board
 		@@y_axis
 	end
 
+	def create_pawns(color)
+		8.times {|i| pawns.push(Pawn.new color)}
+		pawns
+	end
+
+	def create_rooks(color)
+		2.times {|i| rooks.push(Rook.new color)}
+		rooks
+	end
+
+	def create_bishops(color)
+		2.times {|i| bishops.push(Bishop.new color)}
+		bishops
+	end
+
+	def create_knights(color)
+		2.times {|i| knights.push(Knight.new color)}
+		knights
+	end
+
+	def create_queen(color)
+		queens.push(Queen.new color)
+		queens
+	end
+
+	def create_king(color)
+		kings.push(King.new color)
+		kings
+	end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -27,7 +27,7 @@ describe Board do
 			before(:example) {board.create_pawns("black")}
 
 			it "should create 8 black pawns" do
-				expect(board.sqares.values.count {|a| a.is_a? Pawn && a.color == "black"}).to eql(8)
+				expect(board.squares.values.count {|a| a.is_a? Pawn && a.color == "black"}).to eql(8)
 			end
 
 			it "should receive create_rooks with argument black" do
@@ -36,11 +36,11 @@ describe Board do
 
 			before(:example) {board.create_rooks("black")}
 
-			it "should create 2 black rooks"
+			it "should create 2 black rooks" do
 				expect(board.squares.values.count {|a| a.is_a? Rook && a.color == "black"}).to eql(2)
 			end
 
-			it "should receieve create bishops with argument black"
+			it "should receieve create bishops with argument black" do
 				expect(board).to receive(:create_bishops).with("black")
 			end
 
@@ -57,7 +57,7 @@ describe Board do
 			before(:example) {board.create_knights("black")}
 
 			it "should create 2 black knights" do
-				expect(board.sqaures.values.count {|a| a.is_a? Knight && a.color == "black"}).to eql(2)
+				expect(board.squares.values.count {|a| a.is_a? Knight && a.color == "black"}).to eql(2)
 			end
 
 			it "should receive create_queen with argument black" do
@@ -67,7 +67,7 @@ describe Board do
 			before(:example) {board.create_queen("black")}
 
 			it "should create 1 black queen" do
-				expect(board.sqaures.values.count {|a| a.is_a? Queen && a.color == "black"}).to eql(1)
+				expect(board.squares.values.count {|a| a.is_a? Queen && a.color == "black"}).to eql(1)
 			end
 
 			it "should receive create_king with argument black" do
@@ -77,68 +77,68 @@ describe Board do
 			before(:example) {board.create_king("black")}
 
 			it "should create 1 black king" do
-				expect(board.sqaures.values.count {|a| a.is_a? King && a.color == "black"}).to eql(2)
+				expect(board.squares.values.count {|a| a.is_a? King && a.color == "black"}).to eql(2)
 			end
 
 		end
 
 		context "when placing pieces on the board" do
 
-			let(:black_pawn_spaces) {["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"]}
-			let(:white_pawn_spaces) {["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"]}
-			let(:black_rook_spaces) {["A8", "H8"]}
-			let(:white_rook_spaces) {["A1", "H1"]}
-			let(:black_knight_spaces) {["B8", "G8"]}
-			let(:white_knight_spaces) {["B1", "G1"]}
-			let(:black_bishop_spaces) {["C8", "F8"]}
-			let(:white_bishop_spaces) {["C1", "F1"]}
+			let(:black_pawn_squares) {["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"]}
+			let(:white_pawn_squares) {["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"]}
+			let(:black_rook_squares) {["A8", "H8"]}
+			let(:white_rook_squares) {["A1", "H1"]}
+			let(:black_knight_squares) {["B8", "G8"]}
+			let(:white_knight_squares) {["B1", "G1"]}
+			let(:black_bishop_squares) {["C8", "F8"]}
+			let(:white_bishop_squares) {["C1", "F1"]}
 
 			it "should place the black pawns across row 7" do
-				expect(black_pawn_spaces.all? {|sp| board.spaces[sp].is_a? Pawn && board.spaces[sp].color == "black"}).to eql(true)
+				expect(black_pawn_squares.all? {|sp| board.squares[sp].is_a? Pawn && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white pawns across row 2" do
-				expect(white_pawn_spaces.all? {|sp| board.spaces[sp].is_a? Pawn && board.spaces[sp].color == "white"}).to eql(true)
+				expect(white_pawn_squares.all? {|sp| board.squares[sp].is_a? Pawn && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black rooks at A8 and H8" do
-				expect(black_rook_spaces.all? {|sp| board.spaces[sp].is_a? Rook && board.spaces[sp].color == "black"}).to eql(true)
+				expect(black_rook_squares.all? {|sp| board.squares[sp].is_a? Rook && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white rooks at A1 and H1" do
-				expect(white_rook_spaces.all? {|sp| board.spaces[sp].is_a? Rook && board.spaces[sp].color == "white"}).to eql(true)
+				expect(white_rook_squares.all? {|sp| board.squares[sp].is_a? Rook && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black knights at B8 and G8" do
-				expect(black_knight_spaces.all? {|sp| board.spaces[sp].is_a? Knight && board.spaces[sp].color == "black"}).to eql(true)
+				expect(black_knight_squares.all? {|sp| board.squares[sp].is_a? Knight && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white knights at B1 and G1" do
-				expect(white_knight_spaces.all? {|sp| board.spaces[sp].is_a? Knight && board.spaces[sp].color == "white"}).to eql(true)
+				expect(white_knight_squares.all? {|sp| board.squares[sp].is_a? Knight && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black bishops at C8 and F8" do
-				expect(black_bishop_spaces.all? {|sp| board.spaces[sp].is_a? Bishop && board.spaces[sp].color == "black"}).to eql(true)
+				expect(black_bishop_squares.all? {|sp| board.squares[sp].is_a? Bishop && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white bishops at C1 and F1" do
-				expect(white_bishop_spaces.all? {|sp| board.spaces[sp].is_a? Bishop && board.spaces[sp].color == "white"}).to eql(true)
+				expect(white_bishop_squares.all? {|sp| board.squares[sp].is_a? Bishop && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black queen at D8" do
-				expect(board.spaces["D8"].is_a? Queen && board.spaces["D8"].color == "black").to eql(true)
+				expect(board.squares["D8"].is_a? Queen && board.squares["D8"].color == "black").to eql(true)
 			end
 
 			it "should place the white queen at D1" do
-				expect(board.spaces["D1"].is_a? Queen && board.spaces["D1"].color == "white").to wql(true)
+				expect(board.squares["D1"].is_a? Queen && board.squares["D1"].color == "white").to wql(true)
 			end
 
 			it "should place the black king at E8" do
-				expect(board.spaces["E8"].is_a? King && board.spaces["E8"].color == "black").to eql(true)
+				expect(board.squares["E8"].is_a? King && board.squares["E8"].color == "black").to eql(true)
 			end
 
 			it "should place the white king at E1" do
-				expect(board.spaces["E1"].is_a? King && board.spaces["E1"].color == "white").to eql(true)
+				expect(board.squares["E1"].is_a? King && board.squares["E1"].color == "white").to eql(true)
 			end
 		end
 
