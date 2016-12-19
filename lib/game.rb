223ff675194b2aa8e97db78b78981ee39c75b985
Chris Severns <severnsc@gmail.com>
2016-12-19 11:25:52 -0500

    Add .players method

--- lib/game.rb
+++ lib/game.rb
@@ -8,5 +8,9 @@ class Game
 		player1.is_a?(Player) ? @player1 = player1 : raise(TypeError)
 		player2.is_a?(Player) ? @player2 = player2 : raise(TypeError)
 	end
+
+	def players
+		[@player1, @player2]
+	end
 	
 end
