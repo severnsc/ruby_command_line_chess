3f3fe61861d8fcc42b7371d943048b71b46fa75f
Chris Severns <severnsc@gmail.com>
2016-12-19 11:37:03 -0500

    Fix typos in board file

--- lib/board.rb
+++ lib/board.rb
@@ -8,9 +8,13 @@ class Board
 	@@y_axis = (1..8).to_a
 
 	def initialize
-		@pieces = [@pawns, @rooks, @knights, @bishops, @queens, @kings]
-		@pieces.each {|piece| piece = []}
-		create_pawns("black")
+		@pawns = []
+		@rooks = []
+		@knights = []
+		@bishops = []
+		@queens = []
+		@kings = []
+ 		create_pawns("black")
 		create_pawns("white")
 		create_rooks("black")
 		create_rooks("white")
@@ -29,36 +33,36 @@ class Board
 			end
 		end
 		black_pawn_squares = ["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"]
-		black_pawns = pawns.select {|pawn| pawn.color == "black"}
+		black_pawns = @pawns.select {|pawn| pawn.color == "black"}
 		black_pawn_squares.each_with_index {|sq, index| @squares[sq] = black_pawns[index]}
 		white_pawn_squares = ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"]
-		white_pawns = pawns.select {|pawn| pawn.color == "white"}
+		white_pawns = @pawns.select {|pawn| pawn.color == "white"}
 		white_pawn_squares.each_with_index {|sq, index| @squares[sq] = white_pawns[index]}
 		black_rook_squares = ["A8", "H8"]
-		black_rooks = rooks.select {|rook| rook.color == "black"}
+		black_rooks = @rooks.select {|rook| rook.color == "black"}
 		black_rook_squares.each_with_index {|sq, index| @squares[sq] = black_rooks[index]}
 		white_rook_squares = ["A1", "H1"]
-		white_rooks = rooks.select {|rook| rook.color == "white"}
+		white_rooks = @rooks.select {|rook| rook.color == "white"}
 		white_rook_squares.each_with_index {|sq, index| @squares[sq] = white_rooks[index]}
 		black_knight_squares = ["B8", "G8"]
-		black_knights = knights.select {|knight| knight.color == "black"}
+		black_knights = @knights.select {|knight| knight.color == "black"}
 		black_knight_squares.each_with_index {|sq, index| @squares[sq] = black_knights[index]}
 		white_knight_squares = ["B1", "G1"]
-		white_knights = knights.select {|knight| knight.color == "white"}
+		white_knights = @knights.select {|knight| knight.color == "white"}
 		white_knight_squares.each_with_index {|sq, index| @squares[sq] = white_knights[index]}
 		black_bishop_squares = ["C8", "F8"]
-		black_bishops = bishops.select {|bishop| bishop.color == "black"}
+		black_bishops = @bishops.select {|bishop| bishop.color == "black"}
 		black_bishop_squares.each_with_index {|sq, index| @squares[sq] = black_bishops[index]}
 		white_bishop_squares = ["C1", "F1"]
-		white_bishops = bishops.select {|bishop| bishop.color == "white"}
+		white_bishops = @bishops.select {|bishop| bishop.color == "white"}
 		white_bishop_squares.each_with_index {|sq, index| @squares[sq] = white_bishops[index]}
-		black_queen = queens.select {|queen| queen.color == "black"}
+		black_queen = @queens.select {|queen| queen.color == "black"}
 		@squares["D8"] = black_queen[0]
-		white_queen = queens.select {|queen| queen.color == "white"}
+		white_queen = @queens.select {|queen| queen.color == "white"}
 		@squares["D1"] = white_queen[0]
-		black_king = kings.select {|king| king.color == "black"}
+		black_king = @kings.select {|king| king.color == "black"}
 		@squares["E8"] = black_king[0]
-		white_king = kings.select {|king| king.color == "white"}
+		white_king = @kings.select {|king| king.color == "white"}
 		@squares["E1"] = white_king
 	end
 
@@ -71,33 +75,33 @@ class Board
 	end
 
 	def create_pawns(color)
-		8.times {|i| pawns.push(Pawn.new color)}
-		pawns
+		8.times {|i| @pawns.push(Pawn.new color)}
+		@pawns
 	end
 
 	def create_rooks(color)
-		2.times {|i| rooks.push(Rook.new color)}
-		rooks
+		2.times {|i| @rooks.push(Rook.new color)}
+		@rooks
 	end
 
 	def create_bishops(color)
-		2.times {|i| bishops.push(Bishop.new color)}
-		bishops
+		2.times {|i| @bishops.push(Bishop.new color)}
+		@bishops
 	end
 
 	def create_knights(color)
-		2.times {|i| knights.push(Knight.new color)}
-		knights
+		2.times {|i| @knights.push(Knight.new color)}
+		@knights
 	end
 
 	def create_queen(color)
-		queens.push(Queen.new color)
-		queens
+		@queens.push(Queen.new color)
+		@queens
 	end
 
 	def create_king(color)
-		kings.push(King.new color)
-		kings
+		@kings.push(King.new color)
+		@kings
 	end
 
 end
--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,12 @@ require_relative "./pieces.rb"
 require_relative "./board.rb"
 
 class Game
+	attr_reader :board
 
 	def initialize(player1, player2)
 		player1.is_a?(Player) ? @player1 = player1 : raise(TypeError)
 		player2.is_a?(Player) ? @player2 = player2 : raise(TypeError)
+		@board = Board.new
 	end
 
 	def players
