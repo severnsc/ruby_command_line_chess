026b05197bc013a956606fbee542d677c420376d
Chris Severns <severnsc@gmail.com>
2016-12-20 15:31:36 -0500

    Update board specs to fix expect to_receive failing tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/board.rb"
 describe Board do 
 
 	it "should have x-axis markers a-h" do
-		expect(Board.new.x_axis).to eql(("a".."h").to_a)
+		expect(Board.new.x_axis).to eql(("A".."H").to_a)
 	end
 
 	it "should have y-axis markers 1-8" do
@@ -22,62 +22,68 @@ describe Board do
 
 			it "should receive create_pawns with argument black" do
 				expect(board).to receive(:create_pawns).with("black")
+				board.create_pawns("black")
 			end
 
 			before(:example) {board.create_pawns("black")}
 
 			it "should create 8 black pawns" do
-				expect(board.squares.values.count {|a| a.is_a? Pawn && a.color == "black"}).to eql(8)
+				expect(board.squares.values.count {|a| a.is_a?(Pawn) && a.color == "black"}).to eql(8)
 			end
 
 			it "should receive create_rooks with argument black" do
 				expect(board).to receive(:create_rooks).with("black")
+				board.create_rooks("black")
 			end
 
 			before(:example) {board.create_rooks("black")}
 
 			it "should create 2 black rooks" do
-				expect(board.squares.values.count {|a| a.is_a? Rook && a.color == "black"}).to eql(2)
+				expect(board.squares.values.count {|a| a.is_a?(Rook) && a.color == "black"}).to eql(2)
 			end
 
 			it "should receieve create bishops with argument black" do
 				expect(board).to receive(:create_bishops).with("black")
+				board.create_bishops("black")
 			end
 
 			before(:example) {board.create_bishops("black")}
 
 			it "should create 2 black bishops" do
-				expect(board.squares.values.count {|a| a.is_a? Bishop && a.color == "black"}).to eql(2)
+				expect(board.squares.values.count {|a| a.is_a?(Bishop) && a.color == "black"}).to eql(2)
 			end
 
 			it "should receive create_knights with argument black" do
 				expect(board).to receive(:create_knights).with("black")
+				board.create_knights("black")
 			end
 
 			before(:example) {board.create_knights("black")}
 
 			it "should create 2 black knights" do
-				expect(board.squares.values.count {|a| a.is_a? Knight && a.color == "black"}).to eql(2)
+				expect(board.squares.values.count {|a| a.is_a?(Knight) && a.color == "black"}).to eql(2)
 			end
 
 			it "should receive create_queen with argument black" do
 				expect(board).to receive(:create_queen).with("black")
+				board.create_queen("black")
 			end
 
 			before(:example) {board.create_queen("black")}
 
 			it "should create 1 black queen" do
-				expect(board.squares.values.count {|a| a.is_a? Queen && a.color == "black"}).to eql(1)
+				expect(board.squares.values.count {|a| a.is_a?(Queen) && a.color == "black"}).to eql(1)
 			end
 
 			it "should receive create_king with argument black" do
 				expect(board).to receive(:create_king).with("black")
+				board.create_king("black")
 			end
 
 			before(:example) {board.create_king("black")}
 
 			it "should create 1 black king" do
-				expect(board.squares.values.count {|a| a.is_a? King && a.color == "black"}).to eql(2)
+				expect(board.squares.values.count {|a| a.is_a?(King) && a.color == "black"}).to eql(1)
 			end
 
 		end
@@ -94,51 +100,51 @@ describe Board do
 			let(:white_bishop_squares) {["C1", "F1"]}
 
 			it "should place the black pawns across row 7" do
-				expect(black_pawn_squares.all? {|sp| board.squares[sp].is_a? Pawn && board.squares[sp].color == "black"}).to eql(true)
+				expect(black_pawn_squares.all? {|sp| board.squares[sp].is_a?(Pawn) && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white pawns across row 2" do
-				expect(white_pawn_squares.all? {|sp| board.squares[sp].is_a? Pawn && board.squares[sp].color == "white"}).to eql(true)
+				expect(white_pawn_squares.all? {|sp| board.squares[sp].is_a?(Pawn) && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black rooks at A8 and H8" do
-				expect(black_rook_squares.all? {|sp| board.squares[sp].is_a? Rook && board.squares[sp].color == "black"}).to eql(true)
+				expect(black_rook_squares.all? {|sp| board.squares[sp].is_a?(Rook) && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white rooks at A1 and H1" do
-				expect(white_rook_squares.all? {|sp| board.squares[sp].is_a? Rook && board.squares[sp].color == "white"}).to eql(true)
+				expect(white_rook_squares.all? {|sp| board.squares[sp].is_a?(Rook) && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black knights at B8 and G8" do
-				expect(black_knight_squares.all? {|sp| board.squares[sp].is_a? Knight && board.squares[sp].color == "black"}).to eql(true)
+				expect(black_knight_squares.all? {|sp| board.squares[sp].is_a?(Knight) && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white knights at B1 and G1" do
-				expect(white_knight_squares.all? {|sp| board.squares[sp].is_a? Knight && board.squares[sp].color == "white"}).to eql(true)
+				expect(white_knight_squares.all? {|sp| board.squares[sp].is_a?(Knight) && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black bishops at C8 and F8" do
-				expect(black_bishop_squares.all? {|sp| board.squares[sp].is_a? Bishop && board.squares[sp].color == "black"}).to eql(true)
+				expect(black_bishop_squares.all? {|sp| board.squares[sp].is_a?(Bishop) && board.squares[sp].color == "black"}).to eql(true)
 			end
 
 			it "should place the white bishops at C1 and F1" do
-				expect(white_bishop_squares.all? {|sp| board.squares[sp].is_a? Bishop && board.squares[sp].color == "white"}).to eql(true)
+				expect(white_bishop_squares.all? {|sp| board.squares[sp].is_a?(Bishop) && board.squares[sp].color == "white"}).to eql(true)
 			end
 
 			it "should place the black queen at D8" do
-				expect(board.squares["D8"].is_a? Queen && board.squares["D8"].color == "black").to eql(true)
+				expect(board.squares["D8"].is_a?(Queen) && board.squares["D8"].color == "black").to eql(true)
 			end
 
 			it "should place the white queen at D1" do
-				expect(board.squares["D1"].is_a? Queen && board.squares["D1"].color == "white").to wql(true)
+				expect(board.squares["D1"].is_a?(Queen) && board.squares["D1"].color == "white").to eql(true)
 			end
 
 			it "should place the black king at E8" do
-				expect(board.squares["E8"].is_a? King && board.squares["E8"].color == "black").to eql(true)
+				expect(board.squares["E8"].is_a?(King) && board.squares["E8"].color == "black").to eql(true)
 			end
 
 			it "should place the white king at E1" do
-				expect(board.squares["E1"].is_a? King && board.squares["E1"].color == "white").to eql(true)
+				expect(board.squares["E1"].is_a?(King) && board.squares["E1"].color == "white").to eql(true)
 			end
 		end
 
