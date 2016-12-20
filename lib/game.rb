b5c457abcc212b795d684534cae1de7b8307d7c4
Chris Severns <severnsc@gmail.com>
2016-12-19 21:43:39 -0500

    Start building .play_turn. Build conditions for illegal moves.

--- lib/game.rb
+++ lib/game.rb
@@ -9,12 +9,42 @@ class Game
 		player1.is_a?(Player) ? @player1 = player1 : raise(TypeError)
 		player2.is_a?(Player) ? @player2 = player2 : raise(TypeError)
 		@board = Board.new
-		rand(0..1) == 0 ? @player1.color = "white" && @player2.color = "black" : @player1.color = "black" && @player2.color = "white"
-		@current_player = players.select {|p| p.color == "white"}
+		random = rand(0..1)
+		if random == 0
+			@player1.color = "white"
+			@player2.color = "black"
+		else
+			@player1.color = "black"
+			@player2.color = "white"
+		end
+		@player1.color == "white" ? @current_player = @player1 : @current_player = @player2
 	end
 
 	def players
 		[@player1, @player2]
+	end
+
+	def play_turn(start, finish)
+		moving_piece = @board.squares[start]
+		if moving_piece.color == @current_player.color && moving_piece.is_move_legal?(finish)
+			#If the piece on the destination square is an opposing color
+			if @board.squares[finish].color != moving_piece.color
+			#If the piece on the destination square is the same color
+			elsif @board.squares[finish].color == moving_piece.color
+				puts "You already have a piece there! Try again."
+				false
+			#If the square is open
+			else
+				@board.squares[finish] = moving_piece
+				@board.squares[start] = ""
+			end	
+		elsif moving_piece.color != @current_player.color
+			puts "That's not your piece! Try again."
+			false
+		else
+			puts "That move is illegal! Try again."
+			false
+		end
 	end
 
 	def game_over?
