27ed36c462323328beaaa91b0cbfba75857ff115
Chris Severns <severnsc@gmail.com>
2016-12-27 22:50:54 -0500

    Add update_row_column to all piece placement methods

--- lib/board.rb
+++ lib/board.rb
@@ -37,61 +37,73 @@ class Board
 		black_pawn_squares.each_with_index do |sq, index| 
 			@squares[sq] = black_pawns[index]
 			black_pawns[index].current_position = sq
+			black_pawns[index].update_row_column
 		end
 		white_pawn_squares = ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"]
 		white_pawns = @pawns.select {|pawn| pawn.color == "white"}
 		white_pawn_squares.each_with_index do |sq, index| 
 			@squares[sq] = white_pawns[index]
 			white_pawns[index].current_position = sq
+			white_pawns[index].update_row_column
 		end
 		black_rook_squares = ["A8", "H8"]
 		black_rooks = @rooks.select {|rook| rook.color == "black"}
 		black_rook_squares.each_with_index do |sq, index| 
 			@squares[sq] = black_rooks[index]
 			black_rooks[index].current_position = sq
+			black_rooks[index].update_row_column
 		end
 		white_rook_squares = ["A1", "H1"]
 		white_rooks = @rooks.select {|rook| rook.color == "white"}
 		white_rook_squares.each_with_index do |sq, index| 
 			@squares[sq] = white_rooks[index]
 			white_rooks[index].current_position = sq
+			white_rooks[index].update_row_column
 		end
 		black_knight_squares = ["B8", "G8"]
 		black_knights = @knights.select {|knight| knight.color == "black"}
 		black_knight_squares.each_with_index do |sq, index|
 			@squares[sq] = black_knights[index]
 			black_knights[index].current_position = sq
+			black_knights[index].update_row_column
 		end
 		white_knight_squares = ["B1", "G1"]
 		white_knights = @knights.select {|knight| knight.color == "white"}
 		white_knight_squares.each_with_index do |sq, index|
 			@squares[sq] = white_knights[index]
 			white_knights[index].current_position = sq
+			white_knights[index].update_row_column
 		end
 		black_bishop_squares = ["C8", "F8"]
 		black_bishops = @bishops.select {|bishop| bishop.color == "black"}
 		black_bishop_squares.each_with_index do |sq, index|
 			@squares[sq] = black_bishops[index]
 			black_bishops[index].current_position = sq
+			black_bishops[index].update_row_column
 		end
 		white_bishop_squares = ["C1", "F1"]
 		white_bishops = @bishops.select {|bishop| bishop.color == "white"}
 		white_bishop_squares.each_with_index do |sq, index|
 			@squares[sq] = white_bishops[index]
 			white_bishops[index].current_position = sq
+			white_bishops[index].update_row_column
 		end
 		black_queen = @queens.select {|queen| queen.color == "black"}
 		@squares["D8"] = black_queen[0]
 		black_queen[0].current_position = "D8"
+		black_queen[0].update_row_column
 		white_queen = @queens.select {|queen| queen.color == "white"}
 		@squares["D1"] = white_queen[0]
 		white_queen[0].current_position = "D1"
+		white_queen[0].update_row_column
 		black_king = @kings.select {|king| king.color == "black"}
 		@squares["E8"] = black_king[0]
 		black_king[0].current_position = "E8"
+		black_king[0].update_row_column
 		white_king = @kings.select {|king| king.color == "white"}
 		@squares["E1"] = white_king[0]
 		white_king[0].current_position = "E1"
+		white_king[0].update_row_column
 	end
 
 	def x_axis
